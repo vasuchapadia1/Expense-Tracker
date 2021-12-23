37bdc658ef111166c3cfce15fe5b431a58fe1a29
vasuchapadia1 <vasuchapadia804@gmail.com>
2021-12-23 13:24:06 +0530

    Home Component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
+import HomePage from "./components/HomePage";
 
 function App() {
   return (
     <div className="App">
       <h1>Expense Tracker</h1>
+      <HomePage />
     </div>
   );
 }
