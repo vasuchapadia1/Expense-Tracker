9577edb79d85213053482736d54c342c431fbec0
vasuchapadia1 <vasuchapadia804@gmail.com>
2021-12-23 15:06:26 +0530

    NavBar Changes

--- src/App.js
+++ src/App.js
@@ -1,16 +1,11 @@
 import React from "react";
 import "./App.css";
-import { Button } from "@material-ui/core";
-// import HomePage from "./components/HomePage";
+import HomePage from "./components/HomePage";
 // import Paper from "@mui/material/Paper";
 function App() {
   return (
     <div className="App">
-      <Button> Press me </Button>
-      {/* <HomePage /> */}
-      {/* <Paper variant="outlined">
-        <h1 style={{ color: "white" }}>text</h1>
-      </Paper> */}
+      <HomePage />
     </div>
   );
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,28 +1,30 @@
 import React from "react";
-import AppBar from "@mui/material/AppBar";
-import Box from "@mui/material/Box";
-import Toolbar from "@mui/material/Toolbar";
-import Typography from "@mui/material/Typography";
-import IconButton from "@mui/material/IconButton";
+import { makeStyles } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
 
-const Header = () => {
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
+
+export default function Header() {
+  const classes = useStyles();
+
   return (
-    <Box sx={{ flexGrow: 1 }}>
+    <div className={classes.root}>
       <AppBar position="static">
         <Toolbar>
-          <IconButton
-            size="small"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
-          ></IconButton>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+          <Typography variant="h6" className={classes.title}>
             Expense-Tracker
           </Typography>
         </Toolbar>
       </AppBar>
-    </Box>
+    </div>
   );
-};
-export default Header;
+}
